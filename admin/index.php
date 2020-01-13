cf664926f8e6ced468404f7fe9c31202e3ce3a9f
herlianali <aliyasa@umsida.ac.id>
2020-01-13 23:57:34 +0700

    awal

---
+++ admin/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE>
+<html>
+<head>
+	<title>.::liga sepakbola::.</title>
+	<link rel="stylesheet" type="text/css" href="../style/materialize/css/materialize.css">
+</head>
+<body>
+	<form action="" method="POST">
+		<div class="input-field col s6">
+			<div class="row">
+				<div class="col s7">te
+          			<input placeholder="Placeholder" id="first_name" type="text" class="validate">
+          			<label for="first_name">First Name</label>
+				</div>
+			</div>
+        </div>	
+	</form>
+	<link rel="stylesheet" type="text/css" href="style/materialize/js/materialize.js">
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE>
+<html>
+<head>
+	<title>.::liga sepakbola::.</title>
+	<link rel="stylesheet" type="text/css" href="style/materialize/css/materialize.css">
+	<link rel="stylesheet" type="text/css" href="style/materialize/css/icon.css">
+</head>
+<body>
+	<center><h1>Liga Sepakbola</h1></center>
+	<!-- <div class="row">
+		<div class="col s6 m7">
+			<div class="card">
+				<div class="card-content">
+					
+				</div>
+			</div>
+		</div>
+	</div> -->
+<div class="row">
+	<div class="container">
+      <table class="highlight">
+        <thead>
+          <tr>
+              <th>No</th>
+              <th>Klub</th>
+              <th>Kalah</th>
+              <th>Menang</th>
+              <th>Seri</th>
+              <th>Point</th>
+              <th>Gol</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>1.</td>
+            <td>Manchester City</td>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+            <td>9</td>
+            <td>1-10</td>
+          </tr>
+        </tbody>
+      </table>
+	</div>
+</div>
+            
+	<link rel="stylesheet" type="text/css" href="style/materialize/js/materialize.js">
+</body>
+</html>
